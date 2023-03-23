9fee949666b2debcc580cc54bc57da838f55cd83
shk3bq4d <123r4p+github-shk3bq4d-1e8c2700@gmail.com>
2023-03-23 13:02:14 +0100

    graylog_lookup_{adapters,caches,tables}

--- module_utils/graylog.py
+++ module_utils/graylog.py
@@ -1,8 +1,10 @@
+# /* ex: set fenc=utf-8 expandtab ts=4 sw=4 : */
 from ansible.module_utils.urls import Request, to_text
 from urllib.error import URLError
 import urllib
 import json
 import base64
+from pprint import pprint
 
 
 class GraylogApi():
@@ -11,8 +13,8 @@ class GraylogApi():
         self.endpoint = endpoint
         self.username = username
         self.password = password
-        self.session = Request(headers={ 'Content-Type': 'application/json', 'X-Requested-By': 'Graylog API', 'Accept': 'application/json' }, 
-            use_proxy=True, timeout=10, validate_certs=validate_certs)  
+        self.session = Request(headers={ 'Content-Type': 'application/json', 'X-Requested-By': 'Graylog API', 'Accept': 'application/json' },
+            use_proxy=True, timeout=10, validate_certs=validate_certs)
 
     def login(self):
         url = '/api/system/sessions'
@@ -25,13 +27,18 @@ class GraylogApi():
 
     def do_request(self, url, method, payload=None):
         acceptable_status = [200, 201, 204]
+        data=None
+        full_url = self.endpoint + urllib.parse.quote(url)
+        if payload:
+            data=bytes(json.dumps(payload), encoding='utf8')
         try:
-            if not payload:
-                response = self.session.open(method, self.endpoint + urllib.parse.quote(url))
-            else:
-                response = self.session.open(method, self.endpoint + urllib.parse.quote(url), data=bytes(json.dumps(payload), encoding='utf8'))
+            response = self.session.open(method, full_url, data=data)
         except (URLError, ConnectionResetError) as error:
-            raise Exception(error)
+            pprint(vars(error))
+            pprint(error.file.read())
+            pprint(error.fp.read())
+            pprint(vars(error.hdrs))
+            raise Exception(error, f'for url {full_url} and data {data}')
 
         if response.status not in acceptable_status:
             raise Exception('Status {0}, Message {1}'.format(info['msg'], info['body']))
@@ -41,12 +48,23 @@ class GraylogApi():
             content = None
         return content
 
-    def exists(self, item_type, search_key, name):
+    def exists(self, item_type, search_key, name, list_result_keyA=None):
         url = '/api/'+ item_type
         response = self.get(url)
-        for item in response[item_type]:
-            if name == item[search_key]:
-                return True, item
+        if list_result_keyA is None:
+            list_result_keyA = []
+            slash = '/'
+            item_typeA = item_type.split(slash)
+            s = len(item_typeA)
+            for k in range(s):
+                list_result_keyA.append(slash.join(item_typeA[k:s]))
+        elif type(list_result_keyA) == str:
+            list_result_keyA = [list_result_keyA]
+        for item_typeb in list_result_keyA:
+            if item_typeb in response:
+                for item in response[item_typeb]:
+                    if name == item[search_key]:
+                        return True, item
         return False, None
 
     def create(self, url, payload):
